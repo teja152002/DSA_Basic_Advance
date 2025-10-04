6f36a865cd18ec1611a23b3ce7c823a373fb029d
PEESE TEJESWARA RAO <116071058+teja1525@users.noreply.github.com>
2025-10-04 11:34:02 +0530

    Sorting Techniques -> Bubble sort

---
+++ src/mypractice/SortingTechniques.java
@@ -0,0 +1,32 @@
+package mypractice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortingTechniques {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements you want : ");
+        int n = sc.nextInt();
+        System.out.println("Enter array elements to be sorted : ");
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Bubble Sort
+        System.out.println("Bubble Sort");
+        for(int i=0 ;i<n;i++) {
+            for(int j=i+1; j<n;j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
