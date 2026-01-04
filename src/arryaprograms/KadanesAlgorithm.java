da310c5174c4c4f59773dfc3ba669e9ec5a1a54e
Peese Tejeswara Rao <teja152002@gmail.com>
2026-01-04 23:37:51 +0530

    Kadanes Algorithm

--- src/arryaprograms/KadanesAlgorithm.java
+++ src/arryaprograms/KadanesAlgorithm.java
@@ -9,14 +9,36 @@ public class KadanesAlgorithm {
     public static void main(String[] args) {
         int[] arr = {2, 3, -8, 7, -1, 2, 3};
         int maxSum = 0;
+        int arrStart = 0, arrEnd = 0;
+        int sum = 0;
+        int start = 0;
 
+        // Optimal Approach
         for (int i = 0; i < arr.length; i++) {
-            int sum = 0;
-            for (int j = i; j < arr.length; j++) {
-                sum += arr[j];
-                maxSum = Math.max(sum, maxSum);
+            if (sum == 0) start = i;
+            sum += arr[i];
+            if (sum > maxSum) {
+                maxSum = sum;
+                arrStart = start;
+                arrEnd = i;
+            }
+            if (sum < 0) {
+                sum = 0;
             }
         }
+        for (int i = arrStart; i <= arrEnd; i++) {
+            System.out.print(arr[i] + " "); // This is for printing the values of maximum subArray part.
+        }
         System.out.println(maxSum);
+
+//        Better Approach
+//        for (int i = 0; i < arr.length; i++) {
+//            int sum = 0;
+//            for (int j = i; j < arr.length; j++) {
+//                sum += arr[j];
+//                maxSum = Math.max(sum, maxSum);
+//            }
+//        }
+//        System.out.println(maxSum);
     }
 }
